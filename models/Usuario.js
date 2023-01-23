d3fa01c504d3720c1b69a51781600f27eb38968c
Decxo360 <diegolundstedt99@gmail.com>
2023-01-23 18:16:16 -0300

    models

---
+++ models/Usuario.js
@@ -0,0 +1,45 @@
+const {sequelize} = require('../config/bd')
+const {DataTypes}=require('sequelize')
+const { Inmueble } = require('./Inmueble')
+
+const Usuario = sequelize.define('usuario',{
+    idusuario: {
+        type : DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement:true,
+        primaryKey:true
+    },
+    nombre:{
+        type: DataTypes.STRING,
+    },
+    apellido_p:{
+        type:DataTypes.STRING
+    },
+    apellido_m:{
+        type:DataTypes.STRING
+    },
+    correo:{
+        type:DataTypes.STRING,
+        unique:true
+    },
+    contrasena:{
+        type:DataTypes.STRING
+    }
+},{
+    timestamps: false,
+    freezeTableName:true
+})
+
+Usuario.hasMany(Inmueble,{
+    foreignKey:"idusuario",
+    sourceKey:'idusuario'
+})
+
+Inmueble.belongsTo(Usuario,{
+    foreignKey:"idusuario",
+    targetKey:'idusuario'
+})
+
+module.exports={
+    Usuario
+}
