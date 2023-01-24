ad362231d5edc41b8ac1b3bd448830c81bf20907
Decxo360 <diegolundstedt99@gmail.com>
2023-01-23 23:55:48 -0300

    creando model ubicacion mas sus relaciones

---
+++ src/models/Ubicacion.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const {sequelize} = require('../config/bd');
+const { Comuna } = require('./Comuna');
+const { Inmueble } = require('./Inmueble');
+
+const Ubicacion = sequelize.define('region', {
+  idubicacion: {
+    type: DataTypes.INTEGER,
+    primaryKey:true,
+    autoIncrement:true
+  },
+  calle: {
+    type: DataTypes.STRING
+  },
+  numero: {
+    type: DataTypes.INTEGER
+  }
+},{
+    freezeTableName:true
+});
+
+Ubicacion.hasOne(Inmueble,{foreignKey:'idubicacion',sourceKey:'idubicacion'})
+Inmueble.belongsTo(Inmueble,{foreignKey:'ubicacion',targetKey:'ubicacion'})
+
+
+module.exports = {Ubicacion}
