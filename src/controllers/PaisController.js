3e6d4405b31e7d7199947c71ceb7155a1153f777
Decxo360 <diegolundstedt99@gmail.com>
2023-01-24 18:36:39 -0300

    controller, metodo, obtener, eliminar, crear

---
+++ src/controllers/PaisController.js
@@ -0,0 +1,22 @@
+const PaisService = require('../services/PaisService');
+
+const service = new PaisService();
+
+const PaisController = {
+  getAll: async (req, res) => {
+    const paises = await service.getAll();
+    res.json(paises);
+  },
+  deleteById: async (req, res) => {
+    const {id} = req.body;
+    const isDeleted = await service.deleteById(id);
+    res.json(isDeleted);
+  },
+  save: async (req, res) => {
+    const nombre = req.body
+    const isSaved = await service.save(nombre);
+    res.json(isSaved);
+  },
+};
+
+module.exports = PaisController;
