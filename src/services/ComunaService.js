6304c48b5056500d38cc990228ceb4e92e86cd57
Decxo360 <diegolundstedt99@gmail.com>
2023-01-24 18:25:40 -0300

    ComunaService obtener,crear,eliminar

---
+++ src/services/ComunaService.js
@@ -0,0 +1,26 @@
+const {Comuna} = require('../models/Comuna');
+
+class ComunaService {
+  async getComunas() {
+    const comuna = await Comuna.findAll();
+    return comuna
+  }
+  async save(newComuna) {
+    const {nombre,idregion} = newComuna
+    const comuna = await Comuna.create({
+      nombre,
+      idregion
+    });
+    return comuna
+  }
+  async delete(idcomuna) {
+    const comuna = await Comuna.destroy({
+      where: {
+        idcomuna,
+      },
+    })
+    return comuna
+  }
+}
+
+module.exports = ComunaService;
