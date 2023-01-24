9c41062709ddb717cd393793ab4d67ff99f046ff
Decxo360 <diegolundstedt99@gmail.com>
2023-01-24 18:18:02 -0300

    Pais service creado, obtener, guardar, eliminat

---
+++ src/services/PaisService.js
@@ -0,0 +1,25 @@
+const {Pais} = require('../models/Pais');
+
+class PaisService {
+  async getUsers() {
+    const pais = await Pais.findAll();
+    return pais
+  }
+  async save(newPais) {
+    const nombre = newPais
+    const pais = await Pais.create({
+      nombre
+    });
+    return pais
+  }
+  async delete(idpais) {
+    const pais = await Pais.destroy({
+      where: {
+        idpais,
+      },
+    })
+    return pais
+  }
+}
+
+module.exports = PaisService;
